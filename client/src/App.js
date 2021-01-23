2276f3c806cda0f81aa9b33b1936221833ef2455
anthonyvseth <anthonyvseth@gmail.com>
2021-01-23 11:47:42 -0700

    Dashboard setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Dashboard from './components/dashboard/Dashboard'
 import Navbar from './components/layout/Navbar'
+
 
 function App() {
   return (
     <BrowserRouter>
     <div>
       <Navbar />
+      <Switch>
+        <Route path='/' component={Dashboard} />
+      </Switch>
     </div>
     </BrowserRouter>
   );
---
+++ client/src/components/posts/PostList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const PostList = () => {
+    return (
+        <div className='post-list section'>
+
+            <div className='card z-depth-0 post-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Post Title</span>
+                    <p>Posted by Anthony Seth</p>
+                    <p className='grey-text'>23rd January, 11:11</p>
+                </div>
+            </div>
+            <div className='card z-depth-0 post-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Post Title</span>
+                    <p>Posted by Anthony Seth</p>
+                    <p className='grey-text'>23rd January, 11:11</p>
+                </div>
+            </div>
+            <div className='card z-depth-0 post-summary'>
+                <div className='card-content grey-text text-darken-3'>
+                    <span className='card-title'>Post Title</span>
+                    <p>Posted by Anthony Seth</p>
+                    <p className='grey-text'>23rd January, 11:11</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default PostList
