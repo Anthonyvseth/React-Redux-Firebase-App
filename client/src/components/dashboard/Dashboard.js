80d273d1ff7074c557a5cd4846ad987c8457341a
anthonyvseth <anthonyvseth@gmail.com>
2021-01-23 17:16:37 -0700

    post from redux store is mapped through through to post summary

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import PostList from '../posts/PostList'
+import {connect} from 'react-redux'
+
 export class Dashboard extends Component {
     render() {
+        // console.log(this.props)
+        const { posts } = this.props
         return (
             <div className='dashboard container'>
                 <div className='row'>
                     <div className='col s12 m6'>
-                        <PostList />
+                        <PostList posts={posts}/>
                     </div>
                     <div className='col s12 m5 offset-m1'>
                         <Notifications />
@@ -18,4 +22,10 @@ export class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+    return {
+        posts: state.post.posts
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,11 @@
-const initState = {}
+const initState = {
+    posts: [
+        {id:1, title: 'Awesome', content: 'blah blah blah'},
+        {id:2, title: 'Great', content: 'blah blah blah'},
+        {id:3, title: 'Fantastic', content: 'blah blah blah'},
+        {id:4, title: 'Amazing', content: 'blah blah blah'},
+    ]
+}
 
 const postReducer = (state = initState, action) => {
     return state
